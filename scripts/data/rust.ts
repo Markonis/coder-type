f05db0e62500db07d9e38c93bccfce5e025d951f
imrany <imranmat254@gmail.com>
2023-03-31 20:37:45 +0300

    added rust language

--- scripts/data/rust.ts
+++ scripts/data/rust.ts
@@ -5,9 +5,15 @@ export const rustRepo:Repo={
     url: "https://rustlang.org",
     files:[
         {
-            path:"",
+            path:"/home/<username>/.cargo/bin/rustc",
             code:`
-            
+            Rust is a statically-typed programming language designed for performance and safety, \nespecially safe concurrency and memory management.A language empowering everyone 
+to build reliable and efficient software. Example of a rust program:
+
+fn main() {
+    let hello=String::from("Hello world!");
+    println!("{}",hello);
+}
             `
         }
     ]
