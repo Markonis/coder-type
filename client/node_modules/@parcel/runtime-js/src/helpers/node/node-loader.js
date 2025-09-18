4647607e3ec1ac51bfd96b31d336b570f9712b02
Marko Pavlovic <contact.pavlovic@gmail.com>
2025-09-18 14:06:07 +0200

    Remove node_modules from git

--- client/node_modules/@parcel/runtime-js/src/helpers/node/node-loader.js
+++
@@ -1,8 +0,0 @@
-const url = require('url');
-const {createRequire} = require('module');
-
-module.exports = function loadNodeModule(bundle) {
-  let path = url.fileURLToPath(bundle);
-  let require = createRequire(path);
-  return require(path);
-};
