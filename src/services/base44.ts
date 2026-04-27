2f3cb45027584fa3a8f6fedaf54c04f0db2148ce
Marko Pavlovic <contact.pavlovic@gmail.com>
2026-04-27 14:50:10 +0200

    Update app id

--- src/services/base44.ts
+++ src/services/base44.ts
@@ -4,6 +4,6 @@ import { createClient } from "@base44/sdk";
 const NODE_ENV: string = process.env.NODE_ENV;
 const IS_DEV = NODE_ENV === "development";
 export const base44 = createClient({ 
-  appId: "69e0aefb708ee52b433de03a",
+  appId: "69e74a46072cb531b13bf932",
   ...(IS_DEV && { serverUrl: "http://localhost:4400" })
 });
